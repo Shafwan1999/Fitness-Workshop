4b5fc4df3ad032f92368431d3f022202210679f7
Shafwan1999 <shafwan.ahmed1@gmail.com>
2020-11-05 21:15:27 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var nameNode = document.getElementById("name1");
+nameNode.addEventListener("click",function(){
+    alert("© By Shafwan Ahmed");
+});
+
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+
+    burger.addEventListener('click', () => {
+
+        nav.classList.toggle('nav-active');
+
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards  ${index / 7 + 0.5}s`;
+            }
+        });
+
+        burger.classList.toggle('toggle');
+
+    });
+};
+
+navSlide();
